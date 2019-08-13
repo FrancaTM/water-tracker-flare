95d87f14fe949fb040ee56e3caf1627adf5d3137
FrancaTM <francatm@outlook.com>
2019-08-13 14:18:18 -0300

    add methods to tracking input

--- lib/tracking_input.dart
+++ lib/tracking_input.dart
@@ -34,9 +34,70 @@ class _TrackingInputState extends State<TrackingInput> {
   ///per glass (it's assuming)
   static const int ouncePerGlass = 8;
 
+  ///this is a quick reset for the user, to reset the intake back to zero
+  void _resetDay() {
+    setState(() {
+      currentWaterCount = 0;
+      _flareController.resetWater();
+    });
+  }
+
+  ///we'll use this to increase how much water the user has drunk, hooked
+  ///via button
+  void _incrementWater() {
+    setState(() {
+      if (currentWaterCount < selectedGlasses) {
+        currentWaterCount = currentWaterCount + 1;
+
+        double diff = currentWaterCount / selectedGlasses;
+
+        plusWaterControls.play("plus press");
+
+        _flareController.playAnimation("ripple");
+        _flareController.updateWaterPercent(diff);
+      }
+
+      if (currentWaterCount == selectedGlasses) {
+        _flareController.playAnimation("iceboy_win");
+      }
+    });
+  }
+
+  ///we'll use this to decrease our user's water intake, hooked to a button
+  void _decrementWater() {
+    setState(() {
+      if (currentWaterCount > 0) {
+        currentWaterCount = currentWaterCount - 1;
+
+        double diff = currentWaterCount / selectedGlasses;
+
+        _flareController.updateWaterPercent(diff);
+
+        _flareController.playAnimation("ripple");
+      } else {
+        currentWaterCount = 0;
+      }
+
+      minusWaterControls.play("minus press");
+    });
+  }
+
+  void calculateMaxOunces() {
+    maxWaterCount = selectedGlasses * ouncePerGlass;
+  }
+
+  void _incSelectedGlasses(StateSetter updateModal, int value) {
+    updateModal(() {
+      selectedGlasses = (selectedGlasses + value).clamp(0, 26).toInt();
+      calculateMaxOunces();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+
+    _flareController = AnimationControls();
   }
 
   @override
